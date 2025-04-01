7d2669c234b5879b3d4e2c1222d0f4e7322f002e
Samara Angélica <contato.angelica07@gmail.com>
2025-04-01 12:45:00 -0300

    Add files via upload

---
+++ Task 1/Main.js
@@ -0,0 +1,20 @@
+ // -> Our task will be to create a list of contacts. Initially, the list will be quite simple: we will only write three people to it using the data shown in the table below.
+var name;
+var email;
+var phone;
+
+const Contact1 = ("Maxwell Wright");
+const Contact2 = ("Raja Villarreal");
+const Contact3 = ("Helen Richards");
+
+let Contact1Phone = ("(0191) 719 6495");
+let Contact2Phone = ("0866 398 2895");
+let Contact3Phone = ("0800 1111");
+
+let Contact1Email = ("curabitur.egestas.nunc@nonummyanc");
+let Contact2Email = ("posuere.vulputate@sed.com");
+let Contact3Email = ("libero@convallis.edu");
+
+console.log("Contact:", Contact1,"- Phone:", Contact1Phone,"- E-mail:", Contact1Email);
+console.log("Contact:", Contact2,"- Phone:", Contact2Phone,"- E-mail:", Contact2Email);
+console.log("Contact:", Contact3,"- Phone:", Contact3Phone,"- E-mail:", Contact3Email);
